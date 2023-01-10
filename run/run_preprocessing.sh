d09ea486cba8280e998342c7d7c26f3e06325c62
ruishengcao <211314@sjtu.edu.cn>
2023-01-10 15:00:48 +0800

    add preprocessing for three datasets

--- run/run_preprocessing.sh
+++ run/run_preprocessing.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-dataset=spider
-plm=grappa_large_jnt
+plm=roberta-base
 encode_method=none
 
-python3 -u nsts/parse_sql_to_json.py -d $dataset -s all
-python3 -u preprocess/data_preprocess.py -d $dataset -t $plm -e $encode_method -s all
+python3 -u nsts/parse_sql_to_json.py -d spider -s all
+python3 -u nsts/parse_sql_to_json.py -d sparc -s all
+python3 -u nsts/parse_sql_to_json.py -d cosql -s all
+python3 -u preprocess/data_preprocess.py -d spider -t $plm -e $encode_method -s all
+python3 -u preprocess/data_preprocess.py -d sparc -t $plm -e $encode_method -s all
+python3 -u preprocess/data_preprocess.py -d cosql -t $plm -e $encode_method -s all
