b4e13ac9262029e9b4afe01795df4ec01cdd9e0a
ruishengcao <211314@sjtu.edu.cn>
2023-06-14 14:45:39 +0800

    add run/swv.sh

--- run/run_train_and_eval_swv.sh
+++ run/run_train_and_eval_swv.sh
@@ -7,7 +7,7 @@ ddp='' # --ddp
 plm=$2 #electra-small-discriminator
 encode_method=rgatsql
 encoder_hidden_size=256
-encoder_num_layers=2
+encoder_num_layers=8
 num_heads=8
 
 decode_method=ast
@@ -32,7 +32,7 @@ beam_size=5
 n_best=5
 
 python3 -u scripts/train_and_eval.py --task $task --dataset $dataset --seed $seed --device $device $ddp \
-    --encode_method $encode_method --swv $swv --plm $plm --encoder_hidden_size $encoder_hidden_size --encoder_num_layers $encoder_num_layers --num_heads $num_heads \
+    --encode_method $encode_method --swv --plm $plm --encoder_hidden_size $encoder_hidden_size --encoder_num_layers $encoder_num_layers --num_heads $num_heads \
     --decode_method $decode_method --decode_order $decode_order --decoder_cell $decoder_cell --decoder_hidden_size $decoder_hidden_size --decoder_num_layers $decoder_num_layers \
     --dropout $dropout --batch_size $batch_size --test_batch_size $test_batch_size --grad_accumulate $grad_accumulate --eval_after_iter $eval_after_iter --max_iter $max_iter \
     --lr $lr --l2 $l2 --layerwise_decay $layerwise_decay --warmup_ratio $warmup_ratio --lr_schedule $lr_schedule --max_norm $max_norm --beam_size $beam_size --n_best $n_best
